20e6d271767aea771c912621a865a839f9417b7e
Glynn Bird <glynn.bird@gmail.com>
2015-07-03 15:17:28 +0100

    first commit

---
+++ scripts/import.js
@@ -0,0 +1,147 @@
+var linereader = require('line-reader'),
+  http = require('http'),
+  fs = require('fs'),
+  async = require('async'),
+  argv = require('minimist')(process.argv.slice(2)),
+  cloudant = require('cloudant')(process.env.COUCH_URL || "http://127.0.0.1:5984");
+
+if(!argv.title || !argv.author || !argv.year || !argv.id) {
+  console.error("Mandatory command-line parameters:");
+  console.error("  -title : the book's title");
+  console.error("  -author : the book's author");
+  console.error("  -year : the year of publication");
+  console.error("  -id : the Project Gutenberg book id")
+  process.exit();
+}
+var meta = {
+  _id: "" + argv.id,
+  url: "https://www.gutenberg.org/ebooks/" + argv.id,
+  title: argv.title,
+  author: argv.author,
+  year: parseInt(argv.year),
+  db_name: "ebook_" + argv.id,
+  download_url: "http://www.gutenberg.org/cache/epub/" + argv.id + "/pg" + argv.id + ".txt",
+  num_docs: 0
+};
+
+
+
+
+
+
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);
+    });
+  });
+};
+
+
+var db = null,
+  paras = [],
+  filename = "./tmp/" + meta._id + ".txt";
+
+
+async.series([
+  
+  // delete book database
+  function(cb) {
+    console.log("Deleting any pre-existing database for this ebook");
+    cloudant.db.destroy(meta.db_name, function(err, data) {
+      cb(null,null);
+    });
+  },
+  
+  // create and use book database
+  function(cb) {
+    console.log("Creating new database for this ebook");
+    cloudant.db.create(meta.db_name, function(err, data) {
+      db = cloudant.db.use(meta.db_name);
+      cb(null,null);
+    });
+  },
+  
+  // download the book text
+  function(cb) {
+    console.log("Downloading ebook text");
+    fs.exists(filename, function (exists) {
+      if (!exists) {
+        download(meta.download_url, filename, function() {
+          cb(null,null);
+        });
+      } else {
+        cb(null, null);
+      }
+    });
+  },
+  
+  // create document per paragraph
+  function(cb) {
+    console.log("Creating paragraphs");
+    var para = "";
+
+    linereader.eachLine(filename, function(line, last) {
+      line = line.replace("\r","");
+      if (line.length == 0 && para.length>0) {
+        paras.push(para);
+        para = "";
+      } else {
+        if (para.length>0) {
+          para += " ";
+        }
+        para += line;
+      }
+      if (last) {
+        if(paras.length == 0) {
+          console.log("ERROR! Empty file", meta);
+          process.exit();
+        }
+        cb(null,null);
+      }
+    });
+  },
+  
+  // save the data
+  function(cb) {
+    var p = 1000;
+    var docs = [];
+    var txt = "";
+    for(var i in paras) {
+      txt += paras[i];
+      if(i!=0 && i%20==0 || i == paras.length - 1) {
+        var obj = {
+          _id : "" + p++,
+          txt: txt
+        };                
+        docs.push(obj);
+        txt = ""
+      }
+    }
+    db.bulk({docs: docs}, function(err, data) {
+      console.log("Saved",paras.length, "paragraphs in",docs.length,"documents");
+      cb(null,null);
+    })
+  },
+  
+  // create meta data
+  function(cb) {
+    console.log("Creating new meta data");
+    console.log(meta);
+    cloudant.db.create("ebooks", function(err, data) {
+      var ebooksdb = cloudant.db.use("ebooks");
+      ebooksdb.insert(meta, function(err, data) {
+        cb(null, null);
+      });
+    });
+  }
+  
+], function() {
+  console.log("done");
+  process.exit();
+});
+
+
+
---
+++ scripts/import.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Dickens
+node import.js --title "The Pickwick Papers" --author "Charles Dickens" --year 1837 --id 580
+node import.js --title "Nicholas Nickleby" --author "Charles Dickens" --year 1839 --id 967
+node import.js --title "Oliver Twist" --author "Charles Dickens" --year 1839 --id 730
+node import.js --title "The Old Curiosity Shop" --author "Charles Dickens" --year 1841 --id 700
+node import.js --title "Barnaby Rudge" --author "Charles Dickens" --year 1841 --id 917
+node import.js --title "A Christmas Carol" --author "Charles Dickens" --year 1843 --id 46
+node import.js --title "Martin Chuzzlewit" --author "Charles Dickens" --year 1844 --id 968
+node import.js --title "Dombey and Son" --author "Charles Dickens" --year 1848  --id 821
+node import.js --title "David Copperfield" --author "Charles Dickens" --year 1849 --id 766
+node import.js --title "Bleak House" --author "Charles Dickens" --year 1853  --id 1023
+node import.js --title "A Child's History of England" --author "Charles Dickens" --year 1853 --id 699
+node import.js --title "Hard Times" --author "Charles Dickens" --year 1854 --id 786
+node import.js --title "Little Dorrit" --author "Charles Dickens" --year 1857 --id 963
+node import.js --title "A Tale of Two Cities" --author "Charles Dickens" --year 1859 --id 98
+node import.js --title "Great Expectations" --author "Charles Dickens" --year 1861 --id 1400
+node import.js --title "Our Mutual Friend" --author "Charles Dickens" --year 1865 --id 883
+node import.js --title "The Mystery of Edwin Drood" --author "Charles Dickens" --year 1870 --id 564
+
+# Austen
+node import.js --title "Sense and Sensibility" --author "Jane Austen" --year 1811 --id 161
+node import.js --title "Pride and Prejudice" --author "Jane Austen" --year 1813 --id 1342
+node import.js --title "Mansfield Park" --author "Jane Austen" --year 1814 --id 141
+node import.js --title "Emma" --author "Jane Austen" --year 1815 --id 158
+node import.js --title "Northanger Abbey" --author "Jane Austen" --year 1818 --id 121
+node import.js --title "Persuasion" --author "Jane Austen" --year 1818 --id 105
+
+# Brontes
+node import.js --title "Jane Eyre" --author "Charlotte Brontë" --year 1847 --id 1260
+node import.js --title "Wuthering Heights" --author "Emily Brontë" --year 1847 --id 768
+node import.js --title "The Tenant of Wildfell Hall" --author "Anne Brontë" --year 1848 --id 969
+
+# George Eliot
+node import.js --title "Adam Bede" --author "George Eliot" --year 1859 --id 507
+node import.js --title "The Mill on the Floss" --author "George Eliot" --year 1860 --id 6688
+node import.js --title "Silas Marner" --author "George Eliot" --year 1861 --id 550
+node import.js --title "Middlemarch" --author "George Eliot" --year 1871 --id 145
+node import.js --title "Daniel Deronda" --author "George Eliot" --year 1876 --id 7469
+
+# Thomas Hardy
+node import.js --title "Under the Greenwood Tree" --author "Thomas Hardy" --year 1872 --id 2662
+node import.js --title "Far from the Madding Crowd" --author "Thomas Hardy" --year 1874 --id 27
+node import.js --title "The Mayor of Casterbridge" --author "Thomas Hardy" --year 1886 --id 143
+node import.js --title "Tess of the d'Urbervilles" --author "Thomas Hardy" --year 1891 --id 110
+node import.js --title "Jude the Obscure" --author "Thomas Hardy" --year 1895 --id 153
+
+# various 
+node import.js --title "Vanity Fair" --author "William Makepeace Thackeray" --year 1847 --id 599
+node import.js --title "Ivanhoe" --author "Sir Walter Scott" --year 1820 --id 82
+node import.js --title "The Picture of Dorian Gray" --author "Oscar Wilde" --year 1890 --id 174
+node import.js --title "The War of the Worlds" --author "H G Wells" --year 1898 --id 36
+node import.js --title "The Woman in White" --author "Wilkie Collins" --year 1859 --id 583
+
+
+
